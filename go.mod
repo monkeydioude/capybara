e7aef1063c0a6e5bdbb8ee63a30e5c559f3867c0
monkeydioude <duris.julien@gmail.com>
2025-08-30 18:21:01 +0200

    added yaml support for config file

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/monkeydioude/capybara
 
-go 1.22
+go 1.25
 
 toolchain go1.23.1
 
@@ -13,6 +13,7 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/goccy/go-yaml v1.18.0 // indirect
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/net v0.29.0 // indirect
--- internal/capybara/config.go
+++ internal/capybara/config.go
@@ -4,20 +4,23 @@ import (
 	"encoding/json"
 	"log"
 	"os"
+
+	"github.com/goccy/go-yaml"
 )
 
-func NewConfig(p string) (c *Config) {
+func NewConfig(p string) *Config {
 	d, err := os.ReadFile(p)
 
 	if err != nil {
 		log.Fatalf("[ERR ] Could not ReadFile, reason: %s", err)
 	}
+	c := &Config{}
 
-	err = json.Unmarshal(d, &c)
-
-	if err != nil {
-		log.Fatalf("[ERR ] Could not Unmarshal config, reason: %s", err)
+	if err = json.Unmarshal(d, &c); err != nil {
+		if err = yaml.Unmarshal(d, &c); err != nil {
+			log.Fatalf("[ERR ] Could not Unmarshal config, reason: %s", err)
+		}
 	}
 
-	return
+	return c
 }
